c6b1be27f015790be281795ec0338954bd9ca2b4
Folly <60630557+follyfoxe@users.noreply.github.com>
2026-01-04 05:04:10 -0300

    Update FlaskOfAmbrosia.cs

--- Content/Items/Accessories/FlaskOfAmbrosia.cs
+++ Content/Items/Accessories/FlaskOfAmbrosia.cs
@@ -22,8 +22,7 @@ namespace WgMod.Content.Items.Accessories
             if (!player.TryGetModPlayer(out WgPlayer wg))
                 return;
             wg._ambrosiaOnHit = true;
-
-            wg.WeightLossFactor = +2f;
+            wg.WeightLossFactor += 2f;
         }
 
         public override void AddRecipes()
