67c09f0edefb354d2754c84998df13c41eb6b1e8
MaimaiChubsUwU <maimaiowo12705@gmail.com>
2026-03-02 14:32:37 -0800

    Added Caramel Arrow

---
+++ Content/Items/Ammo/CaramelArrow.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WgMod.Content.Items.Consumables;
+
+namespace WgMod.Content.Items.Ammo;
+
+public class CaramelArrow : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 99;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 14;
+        Item.height = 36;
+
+        Item.damage = 14;
+        Item.DamageType = DamageClass.Ranged;
+
+        Item.maxStack = Item.CommonMaxStack;
+        Item.consumable = true;
+        Item.knockBack = 2f;
+        Item.value = Item.sellPrice(copper: 16);
+        Item.shoot = ModContent.ProjectileType<Projectiles.CaramelArrowProjectile>();
+        Item.shootSpeed = 3.5f;
+        Item.ammo = AmmoID.Arrow;
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe(25)
+            .AddIngredient(ItemID.FlamingArrow, 25)
+            .AddIngredient<BottledCaramel>()
+            .Register();
+    }
+}
---
+++ Content/Projectiles/CaramelArrowProjectile.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WgMod.Content.Buffs;
+
+namespace WgMod.Content.Projectiles;
+
+public class CaramelArrowProjectile : ModProjectile
+{
+    public override void SetDefaults()
+    {
+        Projectile.width = 10;
+        Projectile.height = 10;
+
+        Projectile.arrow = true;
+        Projectile.friendly = true;
+        Projectile.DamageType = DamageClass.Ranged;
+        Projectile.timeLeft = 1200;
+    }
+
+    public override void AI()
+    {
+        Projectile.ai[0] += 1f;
+        if (Projectile.ai[0] >= 15f)
+        {
+            Projectile.ai[0] = 15f;
+            Projectile.velocity.Y += 0.1f;
+        }
+
+        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+        if (Projectile.velocity.Y > 16f)
+        {
+            Projectile.velocity.Y = 16f;
+        }
+    }
+
+    public override void OnKill(int timeLeft)
+    {
+        SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+        for (int i = 0; i < 5; i++)
+        {
+            Dust dust = Dust.NewDustDirect(
+                Projectile.position,
+                Projectile.width,
+                Projectile.height,
+                DustID.t_Honey
+            );
+            dust.noGravity = true;
+            dust.velocity *= 1.5f;
+            dust.scale *= 0.9f;
+        }
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        target.AddBuff(ModContent.BuffType<Caramelized>(), 8 * 60);
+    }
+
+    public override void OnHitPlayer(Player target, Player.HurtInfo info)
+    {
+        target.AddBuff(ModContent.BuffType<Caramelized>(), 8 * 60);
+    }
+}
