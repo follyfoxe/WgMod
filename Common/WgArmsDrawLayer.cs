ccee26a2e108731646c3afb20c34ebc32fa2d863
follyfoxe <sweetfollyfox@gmail.com>
2026-03-19 10:44:36 -0300

    Render shouler piece for arms

--- Common/WgArmsDrawLayer.cs
+++ Common/WgArmsDrawLayer.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ModLoader;
 using WgMod.Common.Players;
 
@@ -25,21 +26,25 @@ public class WgArmsDrawLayer : PlayerDrawLayer
         if (armStage < 0)
             return;
 
-        Vector2 vector = new Vector2((int)(drawInfo.Position.X - Main.screenPosition.X - drawInfo.drawPlayer.bodyFrame.Width / 2 + drawInfo.drawPlayer.width / 2), (int)(drawInfo.Position.Y - Main.screenPosition.Y + drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height + 4f)) + drawInfo.drawPlayer.bodyPosition + new Vector2(drawInfo.drawPlayer.bodyFrame.Width / 2, drawInfo.drawPlayer.bodyFrame.Height / 2);
+        Vector2 armPosition = new Vector2((int)(drawInfo.Position.X - Main.screenPosition.X - drawInfo.drawPlayer.bodyFrame.Width / 2 + drawInfo.drawPlayer.width / 2), (int)(drawInfo.Position.Y - Main.screenPosition.Y + drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height + 4f)) + drawInfo.drawPlayer.bodyPosition + new Vector2(drawInfo.drawPlayer.bodyFrame.Width / 2, drawInfo.drawPlayer.bodyFrame.Height / 2);
         Vector2 vector2 = Main.OffsetsPlayerHeadgear[drawInfo.drawPlayer.bodyFrame.Y / drawInfo.drawPlayer.bodyFrame.Height];
         vector2.Y -= 2f;
-        vector += vector2 * -drawInfo.playerEffect.HasFlag(SpriteEffects.FlipVertically).ToDirectionInt();
+        armPosition += vector2 * -drawInfo.playerEffect.HasFlag(SpriteEffects.FlipVertically).ToDirectionInt();
 
+        float bodyRotation = drawInfo.drawPlayer.bodyRotation;
         float rotation = drawInfo.drawPlayer.bodyRotation + drawInfo.compositeFrontArmRotation;
         Vector2 bodyVect = drawInfo.bodyVect;
         Vector2 compositeOffset_FrontArm = new(5 * drawInfo.playerEffect.HasFlag(SpriteEffects.FlipHorizontally).ToDirectionInt(), 0f);
         bodyVect += compositeOffset_FrontArm;
-        vector += compositeOffset_FrontArm;
+        armPosition += compositeOffset_FrontArm;
 
+        Vector2 shoulderPosition = armPosition + drawInfo.frontShoulderOffset;
         if (drawInfo.compFrontArmFrame.X / drawInfo.compFrontArmFrame.Width >= 7)
-            vector -= new Vector2(drawInfo.playerEffect.HasFlag(SpriteEffects.FlipHorizontally).ToDirectionInt(), drawInfo.playerEffect.HasFlag(SpriteEffects.FlipVertically).ToDirectionInt());
+            armPosition -= new Vector2(drawInfo.playerEffect.HasFlag(SpriteEffects.FlipHorizontally).ToDirectionInt(), drawInfo.playerEffect.HasFlag(SpriteEffects.FlipVertically).ToDirectionInt());
 
         SpriteSet.Layer layer = SpriteSet.Current.ArmLayers[armStage];
+        bool drawArmor = WgArmor.ShouldDraw(drawInfo) && layer.UVArmor;
+
         Asset<Texture2D> texture = layer.Texture;
         int frameX = drawInfo.compFrontArmFrame.X / drawInfo.compFrontArmFrame.Width;
         int frameY = drawInfo.compFrontArmFrame.Y / drawInfo.compFrontArmFrame.Height;
@@ -48,21 +53,31 @@ public class WgArmsDrawLayer : PlayerDrawLayer
         bodyVect -= drawInfo.compFrontArmFrame.Size() * 0.5f;
         bodyVect += frame.Size() * 0.5f;
 
-        DrawData drawData = new(
-            texture.Value,
-            vector,
-            frame,
-            drawInfo.colorBodySkin,
-            rotation,
-            bodyVect,
-            1f,
-            drawInfo.playerEffect)
+        if (drawArmor && !drawInfo.compShoulderOverFrontArm)
+            DrawCompShoulder(ref drawInfo, shoulderPosition, bodyRotation, bodyVect);
+
+        DrawData drawData = new(texture.Value, armPosition, frame, drawInfo.colorBodySkin, rotation, bodyVect, 1f, drawInfo.playerEffect)
         {
             shader = drawInfo.skinDyePacked
         };
         drawInfo.DrawDataCache.Add(drawData);
 
-        if (WgArmor.ShouldDraw(drawInfo) && layer.UVArmor)
+        if (drawArmor)
+        {
             WgArmor.Draw(wg, ref drawInfo, drawData, layer);
+            if (drawInfo.compShoulderOverFrontArm)
+                DrawCompShoulder(ref drawInfo, shoulderPosition, bodyRotation, bodyVect);
+        }
+    }
+
+    static void DrawCompShoulder(ref PlayerDrawSet drawInfo, Vector2 position, float bodyRotation, Vector2 bodyVect)
+    {
+        if (drawInfo.hideCompositeShoulders || drawInfo.drawPlayer.body <= 0)
+            return;
+        Texture2D tex = TextureAssets.ArmorBodyComposite[drawInfo.drawPlayer.body].Value;
+        PlayerDrawLayers.DrawCompositeArmorPiece(ref drawInfo, CompositePlayerDrawContext.FrontShoulder, new DrawData(tex, position, drawInfo.compFrontShoulderFrame, drawInfo.colorArmorBody, bodyRotation, bodyVect, 1f, drawInfo.playerEffect)
+        {
+            shader = drawInfo.cBody
+        });
     }
 }
