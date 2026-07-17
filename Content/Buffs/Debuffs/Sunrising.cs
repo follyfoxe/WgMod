24f5063ae42e536587214812e0a6ebbe5ee8d282
MaimaiChubsUwU <maimaiowo12705@gmail.com>
2026-07-16 17:45:13 -0700

    Made Sunrising a debuff

--- Content/Buffs/Debuffs/Sunrising.cs
+++ Content/Buffs/Debuffs/Sunrising.cs
@@ -11,6 +11,10 @@ public class Sunrising : ModBuff
 {
     public override void SetStaticDefaults()
     {
+        Main.debuff[Type] = true;
+        Main.pvpBuff[Type] = true;
+        Main.buffNoSave[Type] = true;
+
         BuffID.Sets.GrantImmunityWith[Type].Add(BuffID.BoneJavelin);
     }
 
